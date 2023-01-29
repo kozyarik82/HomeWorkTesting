62834c2d3f60f55a25acf1c908f239903d4f1022
kozyar <kozyarik@gmail.com>
2023-01-29 03:06:42 +0100

    Решение задачи

--- src/main/java/homework/testing/User.java
+++ src/main/java/homework/testing/User.java
@@ -1,4 +1,33 @@
 package homework.testing;
 
 public class User {
+    private String login;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String login, String email) {
+        this.login = login;
+        String s = "@";
+        String t = ".";
+        if (email == null) {
+            email = "name@email.domen";
+        }
+        if (!email.contains(s) && !email.contains(t)||email.equals(login)) {
+            throw new IncorrectValueException();
+        } else {
+            this.email = email;
+        }
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
+
+
